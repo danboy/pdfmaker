51b9bf1bb91c11931df7eeb97d0ab6c0c29c2a60
Dan Nawara <dnawara@groupon.com>
2011-05-04 22:24:15 -0500

    using print media type

--- app/controllers/pdf_controller.rb
+++ app/controllers/pdf_controller.rb
@@ -6,7 +6,7 @@ class PdfController < ApplicationController
       format.pdf {
         html = Net::HTTP.get_response(URI.parse(params[:url]))
         puts html
-        kit = PDFKit.new(params[:url])
+        kit = PDFKit.new(params[:url], :print_media_type => true)
         #kit.stylesheets << params[:css]
         send_data(kit.to_pdf, :filename => "#{params[:name]}_menu.pdf", :type => 'application/pdf')
         return # to avoid double render call
