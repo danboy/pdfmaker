a27679b0d7b8428cad38aa6d25e909125757a20e
Dan Nawara <dnawara@groupon.com>
2011-05-04 22:00:03 -0500

    first post

---
+++ app/controllers/pdf_controller.rb
@@ -0,0 +1,17 @@
+class PdfController < ApplicationController
+
+  def show
+    respond_to do |format|
+      format.html
+      format.pdf {
+        html = Net::HTTP.get_response(URI.parse(params[:url]))
+        puts html
+        kit = PDFKit.new(params[:url])
+        #kit.stylesheets << params[:css]
+        send_data(kit.to_pdf, :filename => "#{params[:name]}_menu.pdf", :type => 'application/pdf')
+        return # to avoid double render call
+      }
+    end
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pdfgen::Application.config.secret_token = '4efcd9f32a69f8591e9337331a7925ada1ad51cfa48f55b7a4f60e10cb65a5b71b761037a2239dfdf1a861824b26d1bc05107f3c0f5c17dd74c41166ec925b0e'
